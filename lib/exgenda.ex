8fd1bffc6f62c34e2c372d9e28bfeac5d49a2c76
Leonardo Gottardi <leomgottardi@gmail.com>
2021-09-26 22:38:17 -0300

    feat: iniciando agents

--- lib/exgenda.ex
+++ lib/exgenda.ex
@@ -1,9 +1,16 @@
 defmodule Exgenda do
   alias Exgenda.Users.Get, as: GetUser
+  alias Exgenda.Users.Agent, as: UserAgent
   alias Exgenda.Users.CreateOrUpdate, as: CreateOrUpdateUser
 
-  alias Exgenda.Contacts.CreateOrUpdate, as: CreateOrUpdateContact
   alias Exgenda.Contacts.Get, as: GetContact
+  alias Exgenda.Contacts.Agent, as: ContactAgent
+  alias Exgenda.Contacts.CreateOrUpdate, as: CreateOrUpdateContact
+
+  def start_agents do
+    UserAgent.start_link()
+    ContactAgent.start_link()
+  end
 
   defdelegate get_user(params), to: GetUser, as: :call
   defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
